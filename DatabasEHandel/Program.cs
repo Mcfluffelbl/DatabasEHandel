856b3a8dd38367f1398263b338c77980ec66e092
LinneaBlixt <linnea.jmb@gmail.com>
2025-12-01 10:12:43 +0100

    Add project files.

---
+++ DatabasEHandel/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabasEHandel
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
